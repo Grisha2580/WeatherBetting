9236d781311158ae7e8cfd9bdc1d7d69afd66ffb
wstenzel <wills.stenzel@gmail.com>
2019-03-20 16:09:22 -0400

    Made temperature variable not public

--- oracle-contracts/contracts/WeatherOracle.sol
+++ oracle-contracts/contracts/WeatherOracle.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.25;
 
 contract WeatherOracle {
   address public oracleAddress;
-  uint public temperature;
+  uint temperature;
 
   constructor (address _oracleAddress) public {
     oracleAddress = _oracleAddress;
